79ded237c64bcbe2b8f0a40bc91f56374ad84d05
Alexis MF <alexismf2802@gmail.com>
2023-08-07 22:33:48 -0500

    implement deploy

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,51 @@
-#!/usr/bin/env sh
+# Simple workflow for deploying static content to GitHub Pages
+name: Deploy static content to Pages
 
-# abort on errors
-set -e
+on:
+  # Runs on pushes targeting the default branch
+  push:
+    branches: ['main']
 
-#build
-npm run build
+  # Allows you to run this workflow manually from the Actions tab
+  workflow_dispatch:
 
-#navigate into the build output directory
-cd dist
+# Sets the GITHUB_TOKEN permissions to allow deployment to GitHub Pages
+permissions:
+  contents: read
+  pages: write
+  id-token: write
 
-git init
-git add -A
-git commit -m 'deploy'
-https://github.com/alexmf28/File_Select/settings/pages
-git push -f git@github.com:alexmf28/File_Select.git master:gh-pages
+# Allow one concurrent deployment
+concurrency:
+  group: 'pages'
+  cancel-in-progress: true
 
-cd -
+jobs:
+  # Single deploy job since we're just deploying
+  deploy:
+    environment:
+      name: github-pages
+      url: ${{ steps.deployment.outputs.page_url }}
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@v3
+      - name: Set up Node
+        uses: actions/setup-node@v3
+        with:
+          node-version: 18
+          cache: 'npm'
+      - name: Install dependencies
+        run: npm install
+      - name: Build
+        run: npm run build
+      - name: Setup Pages
+        uses: actions/configure-pages@v3
+      - name: Upload artifact
+        uses: actions/upload-pages-artifact@v1
+        with:
+          # Upload dist repository
+          path: './dist'
+      - name: Deploy to GitHub Pages
+        id: deployment
+        uses: actions/deploy-pages@v1
